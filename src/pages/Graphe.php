f2b0e899725adce5912f1818895029aef671aa66
Iddouch Ikram <ikram.iddouch@etu.unilim.fr>
2024-05-21 17:10:03 +0200

    [add] modification du graphe

--- src/pages/Graphe.php
+++ src/pages/Graphe.php
@@ -18,120 +18,122 @@
         <section id="image" class="text-center">
             <img src="assets/graphe2.png" alt="Exemple de graphe">
         </section>
-        <section id="introduction">
-            <h2>Introduction au Graphe de Citation</h2>
-            <p>Un graphe de citation est un outil visuel permettant de représenter les liens entre les articles cités dans vos publications. Il facilite la navigation entre les différents contenus et met en évidence les relations entre eux.</p>
+        <section >
+            <h2>Introduction du sujet</h2>
+            <p>Dans le monde dynamique de la recherche, il est crucial de pouvoir suivre l'évolution des travaux et identifier les tendances émergentes. Notre projet de groupe sur les graphes de citations offre aux enseignants-chercheurs un outil puissant pour visualiser l'impact de leur recherche et explorer les liens entre les publications.</p>
         </section>
-        <section id="fonctionnalites">
-            <h2>Fonctionnalités</h2>
-            <p>Les fonctionnalités du graphe de citation incluent la possibilité de cliquer sur un article pour afficher les articles qu'il cite et ceux qui le citent à leur tour, ainsi que la capacité à explorer les différents clusters thématiques.</p>
+        <section>
+            <h2>Problème</h2>
+            <p>Le suivi traditionnel de la recherche, basé uniquement sur le nombre de publications, ne fournit pas une image complète de l'impact d'un chercheur. Les graphes de citations, en revanche, permettent de visualiser les relations entre les publications et de mesurer l'influence d'un travail sur le domaine de recherche.</p>
         </section>
-        <section id="utilisation">
-            <h2>Utilisation</h2>
-            <p>Le graphe de citation peut être utilisé pour découvrir des articles connexes ou suivre la progression logique des idées à travers vos publications.</p>
+        <section>
+        <h2>Solution</h2>
+
+<p>Notre projet propose une plateforme innovante qui offre aux enseignants-chercheurs les fonctionnalités suivantes :</p>
+
+<ul>
+  <li><strong>Récupération automatique de publications:</strong> Récupérez automatiquement vos publications à partir de bases de données telles que Google Scholar et HAL.</li>
+  <li><strong>Visualisation des graphes de citations:</strong> Construisez des graphes de citations pour visualiser les liens entre vos publications et celles d'autres chercheurs.</li>
+  <li><strong>Suivi de l'évolution de la recherche:</strong> Suivez l'évolution de votre recherche dans le temps et identifiez les tendances émergentes.</li>
+</ul>
+
+
         </section>
-        <section id="comment-acceder">
-            <h2>Comment Accéder au Graphe de Citation</h2>
-            <p>Vous pouvez accéder au graphe de citation en cliquant sur le lien dédié dans le menu de navigation.</p>
-        </section>
-        <script>
-            <?php
-            $sql = 'SELECT pub.title AS titre_publication, b.title AS titre_cite FROM `2025_quotes` JOIN `2025_publications` pub ON `2025_quotes`.id_publication = pub.id JOIN `2025_publications` b ON `2025_quotes`.id_quote = b.id';
+    <div id="container">
+        <div id="graph"></div>
+        <div id="publication-info">Clique sur un noeud pour avoir plus d'information</div>
+    </div>
 
-            // Exécution de la requête SQL
-            $result1 = mysqli_query($conn, $sql);
+    <script>
+    <?php
+    $sql = 'SELECT pub.title AS titre_publication, b.title AS titre_cite , pub.publication_date AS date_publication , b.publication_date AS date_cite, author.firstname AS firstname ,author.lastname AS lastname FROM `2025_quotes` JOIN `2025_publications` pub ON `2025_quotes`.id_publication = pub.id JOIN `2025_publications` b ON `2025_quotes`.id_quote = b.id JOIN `2025_publish` liaison ON `2025_quotes`.id_publication = liaison.id_publication JOIN `2025_authors` author ON liaison.id_author = author.id    ';
+    $result1 = mysqli_query($conn, $sql);
+    $citations = [];
+    while ($row = mysqli_fetch_assoc($result1)) {
+        $citations[] = ['source' => $row['titre_publication'], 'target' => $row['titre_cite']];
+    }
+    $citations_json = json_encode($citations);
+    ?>
+    var citations = <?php echo $citations_json; ?>;
+    var nodes = {};
 
-            // Récupération des résultats
-            $citations = [];
-            while ($row = mysqli_fetch_assoc($result1)) {
-                $citations[] = ['source' => $row['titre_publication'], 'target' => $row['titre_cite']];
-            }
+    citations.forEach(function(link) {
+        link.source = nodes[link.source] || (nodes[link.source] = {name: link.source});
+        link.target = nodes[link.target] || (nodes[link.target] = {name: link.target});
+    });
 
-            // Convertir le tableau PHP en JSON pour l'utiliser en JavaScript
-            $citations_json = json_encode($citations);
-            ?>
-            // Données de citation
-            var citations = <?php echo $citations_json; ?>;
+    var width = 600, height = 600;
 
-            // Créer un dictionnaire des noeuds
-            var nodes = {};
+    var svg = d3.select("#graph").append("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .style("margin", "0 auto")
+        .style("display", "block")
+        .style("background-color", "white");
 
-            // Convertir les liens en objets de noeuds
-            citations.forEach(function(link) {
-                link.source = nodes[link.source] || (nodes[link.source] = {
-                    name: link.source
-                });
-                link.target = nodes[link.target] || (nodes[link.target] = {
-                    name: link.target
-                });
+    // Define arrow markers for graph links
+    svg.append('defs').append('marker')
+        .attr('id', 'arrowhead')
+        .attr('viewBox', '-0 -5 10 10')
+        .attr('refX', 13)
+        .attr('refY', 0)
+        .attr('orient', 'auto')
+        .attr('markerWidth', 13)
+        .attr('markerHeight', 13)
+        .attr('xoverflow', 'visible')
+        .append('svg:path')
+        .attr('d', 'M 0,-5 L 10 ,0 L 0,5')
+        .attr('fill', '#999')
+        .style('stroke','none');
+
+    var simulation = d3.forceSimulation()
+        .nodes(d3.values(nodes))
+        .force("link", d3.forceLink(citations).distance(60).id(function(d) { return d.name; }))
+        .force("charge", d3.forceManyBody().strength(-300))
+        .force("center", d3.forceCenter(width / 2, height / 2))
+        .on("tick", tick);
+
+    var link = svg.selectAll(".link")
+        .data(citations)
+        .enter().append("line")
+        .attr("class", "link")
+        .attr("marker-end", "url(#arrowhead)") // Use this line for directed graph arrows
+        .style("stroke", "#999")
+        .style("stroke-width", "0.5px");
+
+        var node = svg.selectAll(".node")
+            .data(simulation.nodes())
+            .enter().append("circle")
+            .attr("class", "node")
+            .attr("r", 5)  // Rayon du cercle
+            .on("mouseover", function(d) {
+                // Afficher le nom du titre lorsque la souris passe sur le nœud
+                svg.append("text")
+                    .attr("id", "tooltip")
+                    .attr("x", d.x)
+                    .attr("y", d.y)
+                    .text(d.name);
+            })
+            .on("mouseout", function(d) {
+                // Enlever le nom du titre lorsque la souris quitte le nœud
+                d3.select("#tooltip").remove();
+            })
+            .on("click", function(d) {
+            // Afficher le nom complet de la publication dans une div à côté du graphe
+            var publicationInfo = document.getElementById("publication-info");
+            publicationInfo.innerHTML = "<h3>Nom complet de la publication :</h3>" +
+                                        "<p>" + d.name + "</p>";
             });
+    function tick() {
+        link.attr("x1", function(d) { return d.source.x; })
+            .attr("y1", function(d) { return d.source.y; })
+            .attr("x2", function(d) { return d.target.x; })
+            .attr("y2", function(d) { return d.target.y; });
 
-            var width = 800,
-                height = 600;
-
-            var svg = d3.select("body").append("svg")
-                .attr("width", width)
-                .attr("height", height);
-
-            var simulation = d3.forceSimulation()
-                .nodes(d3.values(nodes))
-                .force("link", d3.forceLink(citations).distance(60))
-                .force("charge", d3.forceManyBody().strength(-300))
-                .force("center", d3.forceCenter(width / 2, height / 2))
-                .on("tick", tick);
-
-            var link = svg.selectAll(".link")
-                .data(citations)
-                .enter().append("line")
-                .attr("class", "link")
-                .style("stroke", "#999") // Couleur des liens
-                .style("stroke-width", "2px"); // Épaisseur des liens
-
-            var node = svg.selectAll(".node")
-                .data(simulation.nodes())
-                .enter().append("circle")
-                .attr("class", "node")
-                .attr("r", 5) // Rayon du cercle
-                .on("mouseover", function(d) {
-                    // Afficher le nom du titre lorsque la souris passe sur le nœud
-                    svg.append("text")
-                        .attr("id", "tooltip")
-                        .attr("x", d.x)
-                        .attr("y", d.y)
-                        .text(d.name);
-                })
-                .on("mouseout", function(d) {
-                    // Enlever le nom du titre lorsque la souris quitte le nœud
-                    d3.select("#tooltip").remove();
-                })
-                .on("click", function(d) {
-                    // Action à effectuer lorsque le nœud est cliqué
-                    alert("Vous avez cliqué sur : " + d.name);
-                });
-
-            // Mise à jour de la position des noeuds et des liens
-            function tick() {
-                link.attr("x1", function(d) {
-                        return d.source.x;
-                    })
-                    .attr("y1", function(d) {
-                        return d.source.y;
-                    })
-                    .attr("x2", function(d) {
-                        return d.target.x;
-                    })
-                    .attr("y2", function(d) {
-                        return d.target.y;
-                    });
-
-                node.attr("cx", function(d) {
-                        return d.x;
-                    })
-                    .attr("cy", function(d) {
-                        return d.y;
-                    });
-            }
-        </script>
+        node.attr("cx", function(d) { return d.x; })
+            .attr("cy", function(d) { return d.y; });
+    }
+</script>
     </main>
     <?php require_once("./src/components/footer.php"); ?>
 </body>
